5e3590d8dde56c7eff20f750d619935c684c0341
aniket123de <aniketde1234@gmail.com>
2026-02-23 18:16:42 +0530

    fixing

--- src/components/Pages/Team/Team.js
+++ src/components/Pages/Team/Team.js
@@ -39,21 +39,6 @@ const Teams = () => {
             <div className="container">
               <div className="row align-items-center">
                 <div className="col-xl-12 col-lg-12">
-                  <div className="breadcrumb-wrap text-left">
-                    <div className="breadcrumb-title">
-                      <h2>Team</h2>
-                      <div className="breadcrumb-wrap">
-                        <nav aria-label="breadcrumb">
-                          <ol className="breadcrumb">
-                            <li className="breadcrumb-item">
-                              <Link to="/">Home</Link>
-                            </li>
-                            <li className="breadcrumb-item active" aria-current="page">Team</li>
-                          </ol>
-                        </nav>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
